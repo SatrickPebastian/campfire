fa1b2131c2bbdcd504c5318216ac2ac8570d209b
SatrickPebastian <patrick.stopper@outlook.de>
2023-11-03 20:16:56 +0100

    Fix campfire frontend port

--- campfire-frontend/vite.config.ts
+++ campfire-frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4137, // Set the desired port here
+    port: 4173, // Set the desired port here
   }
 })
